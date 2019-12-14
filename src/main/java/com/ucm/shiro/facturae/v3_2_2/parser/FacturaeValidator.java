1dabe172b1d5b21b9e3b847571855ba975c66c75
Roberto Pintos López <roberto@secture.com>
2019-12-15 00:02:27 +0100

    Added Facturae validators (3.1 to 3.2.2)

---
+++ src/main/java/com/ucm/shiro/facturae/v3_2_2/parser/FacturaeValidator.java
@@ -0,0 +1,17 @@
+package com.ucm.shiro.facturae.v3_2_2.parser;
+
+import com.ucm.shiro.facturae.agnostic.parser.AbstractFacturaeValidator;
+import com.ucm.shiro.facturae.agnostic.parser.FacturaeValidationException;
+
+public class FacturaeValidator extends AbstractFacturaeValidator {
+
+    private static final String FACTURAE_XSD_PATH = "xml/schema/facturae/v3_2_2/Facturae.xsd";
+    private static final String CORE_XSD_PATH = "xml/schema/facturae/v3_2_2/xmldsig-core-schema.xsd";
+
+    public FacturaeValidator() throws FacturaeValidationException {
+        super(
+            FacturaeValidator.class.getClassLoader().getResourceAsStream(FACTURAE_XSD_PATH),
+            FacturaeValidator.class.getClassLoader().getResourceAsStream(CORE_XSD_PATH)
+        );
+    }
+}
